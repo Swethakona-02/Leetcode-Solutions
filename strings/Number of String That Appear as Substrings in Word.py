772215b44169459a9e30e8bc149b0e2802cec9a8
Swethakona-02 <swethakona1008@gmail.com>
2026-08-12 18:58:32 +0530

    Delete Number of String That Appear as Substrings in Word.py

--- strings/Number of String That Appear as Substrings in Word.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def numOfStrings(self, patterns: List[str], word: str) -> int:
-        cnt=0
-        for pattern in patterns:
-            if pattern in word:
-                cnt+=1
-        return cnt
